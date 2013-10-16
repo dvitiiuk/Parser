111a6c055d08e34a7cce2cdc5021c9a42d3b710c
msafronenko <msafronenko@hortonworks.com>
2013-10-16 15:43:16 +0300

    Change column name

--- src/TableView.java
+++ src/TableView.java
@@ -5,7 +5,7 @@ public class TableView extends JFrame {
     private JTable table;
     public TableView(String title, Object[][] cellValues) throws HeadlessException {
         super(title);
-        table = new JTable(cellValues, new String[]{"Method name", "Test failure details"});
+        table = new JTable(cellValues, new String[]{"Class name", "Test failure details"});
         JScrollPane scrollPane = new JScrollPane(table);
         table.setFillsViewportHeight(true);
         setLayout(new BorderLayout());
